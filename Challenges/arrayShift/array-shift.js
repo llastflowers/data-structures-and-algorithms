097f85d569c264598eed3eb1f77f4158a2b7842f
Brittany Houtz <brittany.houtz@gmail.com>
2019-12-04 13:11:34 -0800

    added comment

--- Challenges/arrayShift/array-shift.js
+++ Challenges/arrayShift/array-shift.js
@@ -9,4 +9,4 @@ function insertShiftArray(arr, newItem) {
 
 console.log(insertShiftArray([1, 2, 3, 4, 5, 6], 3.5));
 
-// I'm stuck at this point! This code is returning an array that inserts newItem into the array up to the midpoint, but does not preserve the original array items and inserts newItem 3 times ([3.5, 3.5, 3.5] in the case of the above console.log)
+// I'm stuck at this point! This code is returning an array that inserts newItem into the array up to the midpoint, but does not preserve the original array items and inserts newItem 3 times ([3.5, 3.5, 3.5] in the case of the above console.log). I had a slightly different partial-solution that was turning the whole array into [3.5]; I'm not sure which was closer to working properly. I spent about 1.5 hours on this.
