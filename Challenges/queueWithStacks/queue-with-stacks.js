3fb90152d065ec2e592f41ca5efb53de29752784
Brittany Houtz <brittany.houtz@gmail.com>
2019-12-17 14:30:02 -0800

    attempted some more pseudoqueue stuff and stuck on weird test issue...

--- Challenges/queueWithStacks/queue-with-stacks.js
+++ Challenges/queueWithStacks/queue-with-stacks.js
@@ -40,19 +40,27 @@ class PseudoQueue {
     }
 
     enqueue(value) {
-        while (!this.stack1.isEmpty) {
-            this.stack1.push(this.stack2.pop(value));   
+        while (!this.stack1.isEmpty()) {
+            const top = this.stack1.pop(this.stack1.top);
+            this.stack2.push(top);   
         }
+        const node = new Node(value, null);
+        this.stack2.push(node);
+
+        while (!this.stack2.isEmpty()){
+            const top = this.stack2.pop(this.stack2.top);
+            this.stack1.push(top);
+        }
+        return this.stack1;
     }
 
-    dequeue(value) {
+    // dequeue(value) {
         
-    }
+    // }
 
     toString() {
-        return this.stack1.toString()
-            .concat(' -> ');
+        return this.stack1.toString();
     }
 }
 
-module.exports = { PseudoQueue, Stack, Node };
+module.exports = { PseudoQueue };
