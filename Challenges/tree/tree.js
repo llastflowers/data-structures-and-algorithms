b9ab800004934ce607dec82dccba3c591adc67fb
Brittany Houtz <brittany.houtz@gmail.com>
2020-01-28 21:54:44 -0800

    first two tests passing

--- Challenges/tree/tree.js
+++ Challenges/tree/tree.js
@@ -1,45 +1,77 @@
 class Node {
-    constructor(value, leftChild, rightChild){
-
-    }
-
-    add() {
-
-    }
-
-    contain(){
-
-    }
+  constructor(value, leftChild = null, rightChild = null){
+    this.value = value;
+    this.leftChild = leftChild;
+    this.rightChild = rightChild;
+  }
 }
 
 class BinaryTree {
-    constructor(){
+  constructor(){
 
-    }
+  }
 
-    preOrder(){
+  preOrder(){
 
-    }
+  }
 
-    inOrder(){
+  inOrder(){
 
-    }
+  }
 
-    postOrder(){
+  postOrder(){
 
-    }
+  }
 }
 
 class BinarySearchTree {
-    constructor(){
+  constructor(){
+    this.root = null;
+  }
 
+  add(value) {
+    const node = this.root;
+    if(node === null) {
+      this.root = new Node(value);
+      return;
+    } else {
+      const addNode = function(node) {
+        if(value < node.value) {
+          if(node.leftChild === null) {
+            node.leftChild = new Node(value);
+            return;
+          } else if(node.leftChild !== null) {
+            return addNode(node.leftChild);
+          }
+        } else if(value > node.value) {
+          if(node.rightChild === null) {
+            node.rightChild = new Node(value);
+            return;
+          } else if(node.rightChild !== null) {
+            return addNode(node.rightChild);
+          }
+        } else {
+          return null;
+        }
+      };
+      return addNode(node);
     }
+  }
 
-    add(value) {
-
+  contains(value){
+    let current = this.root;
+    while(current) {
+      if(value === current.value) {
+        return true;
+      }
+      if(value < current.value) {
+        current = current.leftChild;
+      } else {
+        current = current.right;
+      }
     }
-
-    contain(value){
-
-    }
+    return false;
+  }
 }
+
+module.exports = { Node, BinarySearchTree };
--- Challenges/tree/tree.test.js
+++ Challenges/tree/tree.test.js
@@ -1,27 +1,29 @@
-// const { } = require('./tree');
+const { BinarySearchTree } = require('./tree');
 
 describe('Binary Tree', () => {
-    it('Can successfully instantiate an empty tree', () => {
-        expect().toEqual();
-    });
+  it('Can successfully instantiate an empty tree', () => { const tree = new BinarySearchTree();
+    expect(tree).toBeNull;
+  });
 
-    it('Can successfully instantiate a tree with a single root node', () => {
-        expect().toEqual();
-    });
+  it('Can successfully instantiate a tree with a single root node', () => {
+    const tree = new BinarySearchTree();
+    tree.add(5);
+    expect(tree.root).toEqual({ 'leftChild': null, 'rightChild': null, 'value': 5 });
+  });
 
-    it('Can successfully add a left child and right child to a single root node', () => {
-        expect().toEqual();
-    });
+  it('Can successfully add a left child and right child to a single root node', () => {
+    expect().toEqual();
+  });
 
-    it('Can successfully return a collection from a preorder traversal', () => {
-        expect().toEqual();
-    });
+  it('Can successfully return a collection from a preorder traversal', () => {
+    expect().toEqual();
+  });
 
-    it('Can successfully return a collection from an inorder traversal', () => {
-        expect().toEqual();
-    });
+  it('Can successfully return a collection from an inorder traversal', () => {
+    expect().toEqual();
+  });
 
-    it('Can successfully return a collection from a postorder traversal', () => {
-        expect().toEqual();
-    });
+  it('Can successfully return a collection from a postorder traversal', () => {
+    expect().toEqual();
+  });
 });
