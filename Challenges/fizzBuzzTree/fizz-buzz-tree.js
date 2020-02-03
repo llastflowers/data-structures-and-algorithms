f5ec5107ecb5bf054ec99e1badfa4a52fdf1f214
Brittany Houtz <brittany.houtz@gmail.com>
2020-02-02 21:48:42 -0800

    partial solution

--- Challenges/fizzBuzzTree/fizz-buzz-tree.js
+++ Challenges/fizzBuzzTree/fizz-buzz-tree.js
@@ -1,7 +1,18 @@
-function FizzBuzzTree() {
+function FizzBuzzTree(tree) {
+  let node;
+  if(node.value % 3 === 0) {
+    node.value = 'Fizz';
+  } else if(node.value % 5 === 0) {
+    node.value = 'Buzz';
+  } else if(node.value % 3 === 0 && node.value & 5 === 0) {
+    node.value = 'FizzBuzz';
+  } else node.value = node.value.toString();
+  
   // takes a tree as arg
   // if node.value / 3 = 0, node.value = 'Fizz'
   // if node.value / 5 = 0, node.value = 'Buzz'
   // if node.value / 3 = 0 AND node.value / 5 = 0, node.value = 'Fizzbuzz'
   // else node.value = node.value.toString()  
 }
+
+module.exports = { FizzBuzzTree };
